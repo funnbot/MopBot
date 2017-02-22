ac51f79e94e4721ed1c10b30d2ecb29d812b69bb
funnbot <unconfigured@null>
2017-02-22 11:06:59 -0800

    Updating Modules

---
+++ src/commands/purge.js
@@ -0,0 +1,15 @@
+const Command = require('../struct/Command');
+
+module.exports = new Command('purge')
+    .setCategory('moderation')
+    .setUsage('Delete messages in a channel')
+    .alias(["prune", "clear"])
+    .inDm(false)
+    .userPerms(['MANAGE_MESSAGES'])
+    .botPerms(['MANAGE_MESSAGES'])
+    .setArgs([])
+    .setExec((message, bot) => {
+
+        
+
+    })
